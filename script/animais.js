b6e78690b6576f8ae64f88350d16c10c4218c659
unknown <beatrizdecatrom98@outlook.com>
2024-11-24 16:34:08 -0300

    ajuste dos links do header e js correspondente

--- script/animais.js
+++ script/animais.js
@@ -2,15 +2,62 @@ import {produtos} from './bancoDeDados.js';
 
 const params = new URLSearchParams(window.location.search);
 const idCategoria = params.get('categoria');
-
+if(idCategoria === 'Suprimentos'){
+    exibirPaginaPorTipo(produtos, idCategoria, '.animais-tipo');
+}
 if(idCategoria){
     console.log('ID categoria: ', idCategoria);
 }else{
     console.log('Não há id de produto na url');
-    exibirPaginaAnimais();
+    exibirPaginaPorTipo(produtos, 'animal', '.animais-tipo' );
 }
 
-function exibirPaginaAnimais(){
+//formatando o preço
+function formatarPreco(preco){
+    return new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    }).format(preco)
+}
+
+function exibirPaginaPorTipo(produtos, tipo, containerSeletor){
+    injetarBannerHtml();
+    //selecionando o container
+    const container = document.querySelector(containerSeletor);
+
+    if(!container){
+        console.error(`container com seletor "${container} não encontrado"`);
+        return;
+    }
+
+    //filtrar categoria
+    const produtosFiltrados = produtos.filter(produto => {
+        return produto.tipo === tipo;
+    });
+
+    if(!produtosFiltrados){
+        produtosFiltrados == produtos.filter(produto =>{
+            return produto.tipo !== 'animais';
+        });
+    }
+
+    //gerando e injetando no html
+    produtosFiltrados.forEach(produto => {
+        const produtoHTML = `
+        <div class="animais-tipo-item">
+            <a href="./animais.html?p=${produto.id}">
+                <img src="${produto.imagem}" alt="${produto.titulo}">
+            </a>
+            <p>${produto.titulo}</p>
+            <h3>${formatarPreco(produto.preco)}</h3>
+            <p>Em até 5x no cartão.</p>
+            class="button">Adicionar ao carrinho</a>
+        </div>
+        `;
+        container.innerHTML +=produtoHTML;
+    });
     
 }
 
@@ -31,6 +78,7 @@ function injetarBannerHtml(){
     container.innerHTML += bannerHtml;
 }
 
+
 function injetandoProdutosNoHtml(produtos, categoria, containerSeletor){
 
     //selecionando o container
@@ -45,16 +93,6 @@ function injetandoProdutosNoHtml(produtos, categoria, containerSeletor){
     const produtosFiltrados = produtos.filter(produto => {
         return produto.categoria === categoria;
     });
-
-    //formatando o preço
-    function formatarPreco(preco){
-        return new Intl.NumberFormat('pt-BR', {
-            style: 'currency',
-            currency: 'BRL',
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 2
-        }).format(preco)
-    }
 
     //gerando e injetando no html
     produtosFiltrados.forEach(produto => {
